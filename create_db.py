25152c789681a7b0d86f7582551c1f0d2848b4cf
jarayl <pixelated998@gmail.com>
2023-12-01 22:25:07 -0500

    db

--- create_db.py
+++ create_db.py
@@ -2,4 +2,12 @@ import sqlite3
 
 db = sqlite3.connect('skincheck.db')
 
-db.execute('CREATE TABLE users ()')
+db.execute('CREATE TABLE docs (dId TEXT, name TEXT, dEmail TEXT)')
+db.execute('CREATE TABLE docPatRel (dId TEXT, pId TEXT)')
+db.execute('CREATE TABLE pats (pId TEXT, name TEXT, pEmail TEXT, gender TEXT, age INTEGER, password TEXT)')
+db.execute('CREATE TABLE data (dId TEXT, pId TEXT, diagnosis TEXT, desc TEXT, date DATETIME, img BLOB, overlay BLOB)')
+
+print("done!")
+
+db.close()
+
