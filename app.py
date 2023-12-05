508330e7e8cc8db724361f9ec2ec3ccfc724147a
SahilS123 <sahilsood2005@gmail.com>
2023-12-05 12:30:19 -0500

    changes

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required, doctor_only, patient_only
+
 import sqlite3
 
-app = Flask(__name__)
-
+app = Flask(__name__)  
+app.secret_key = "sahil dick small"
 #additional functionalities: verify email, password must have some level of security, settings, etc
 Session(app)
 
@@ -27,6 +29,7 @@ def index():
 def doctor_login():
     if request.method == "POST":
         #TODO
+        print()
     else:
         return render_template("doctor_login.html")
 
@@ -35,6 +38,7 @@ def doctor_login():
 def patient_login():
     if request.method == "POST":
         #TODO
+        print()
     else:
         return render_template("patient_login.html")
 
@@ -43,6 +47,7 @@ def patient_login():
 def doctor_register():
     if request.method == "POST":
         #TODO
+        print()
     else:
         return render_template("doctor_register.html")
 
@@ -51,6 +56,7 @@ def doctor_register():
 def patient_register():
     if request.method == "POST":
         #TODO
+        print()
     else:
         return render_template("patient_register.html")
 
@@ -58,17 +64,19 @@ def patient_register():
 @login_required
 @doctor_only
 def doctor_home():
-
+    return TODO
 
 @app.route("/patient/home") #patient history
 @patient_only
 def patient_home():
+    return TODO
 
 
 @app.route("/doctor/upload") #place to upload the image for AI model
 @login_required
 @doctor_only
 def img_upload():
+    return TODO
     
 
 @app.route("/logout")
