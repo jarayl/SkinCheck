cf7cee3806af38d72c12ef25acd9be7624cf4d13
SahilS123 <sahilsood2005@gmail.com>
2023-12-05 12:33:52 -0500

    .

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -6,19 +6,7 @@ from helpers import login_required, doctor_only, patient_only
 
 import sqlite3
 
-<<<<<<< HEAD
 app = Flask(__name__)  
-app.secret_key = "sahil dick small"
-=======
-db = sqlite3.connect("skincheck.db")
-cur = db.cursor()
-
-app = Flask(__name__)
-app.secret_key = "JustinJaraySahil"
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-
->>>>>>> main
 #additional functionalities: verify email, password must have some level of security, settings, etc
 Session(app)
 
@@ -35,50 +23,25 @@ def index():
     else:
         return render_template("index.html")
 
-@app.route("/doctor/login", methods=["GET", "POST"])
+@app.route("/doctor/login")
 @doctor_only
 def doctor_login():
     if request.method == "POST":
         #TODO
-<<<<<<< HEAD
         print()
-=======
-        # Ensure patient email was submitted
-        if not request.form.get("email"):
-            return 
-        # Ensure patient password was submitted
-        elif not request.form.get("password"):
-            return 
-        #sql logic to make sure that user exists in db
-
-        return redirect("/doctor/home")
->>>>>>> main
     else:
         return render_template("doctor_login.html")
 
-@app.route("/patient/login", methods=["GET", "POST"])
+@app.route("/patient/login")
 @patient_only
 def patient_login():
     if request.method == "POST":
         #TODO
-<<<<<<< HEAD
         print()
-=======
-        # Ensure patient email was submitted
-        if not request.form.get("email"):
-            return 
-        
-        # Ensure patient password was submitted
-        elif not request.form.get("password"):
-            return 
-        
-        #sql logic to make sure patient exists in db
-        return redirect("/patient/home")
->>>>>>> main
     else:
         return render_template("patient_login.html")
 
-@app.route("/doctor/register", methods=["GET", "POST"])
+@app.route("/doctor/register")
 @doctor_only
 def doctor_register():
     if request.method == "POST":
@@ -96,48 +59,28 @@ def patient_register():
     else:
         return render_template("patient_register.html")
 
-@app.route("/doctor/home", methods=["GET", "POST"]) #table of patients with uploaded images
+@app.route("/doctor/home") #table of patients with uploaded images
 @login_required
 @doctor_only
 def doctor_home():
-<<<<<<< HEAD
     return TODO
-=======
-    if request.method == "POST":
-        #form should have value of patient id
-        pId = request.form.get("pId")
-        return redirect(url_for("patient_history", patient_id = pId))
-    else:
-        return render_template("doctor_home.html")
->>>>>>> main
 
 @app.route("/patient/home") #patient history
-@login_required
 @patient_only
 def patient_home():
     return TODO
 
-@app.route("/doctor/upload", methods=["GET", "POST"]) #place to upload the image for AI model
+
+@app.route("/doctor/upload") #place to upload the image for AI model
 @login_required
 @doctor_only
 def img_upload():
     return TODO
-<<<<<<< HEAD
     
-=======
-
-@app.route("/doctor/<patient_id>", methods=["GET", "POST"]) #implement downloading image
-@login_required
-@doctor_only
-def patient_hist(patient_id):
-    #some sql stuff that gets the patient history with doctor id and patient id
-    return render_template("patient_history.html")
->>>>>>> main
 
 @app.route("/logout")
 def logout():
     session.clear()
-    flash("Logout Successful!")
     return redirect("/")
 
 
